957ac9f85ccfcb800d2fc1706335cc3747d26b16
Philippe Cuaz <philippe.cuaz@orange.fr>
2023-05-05 15:16:46 +0200

    first commit

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loading = () => {
+  console.log("loading...");
+  return (
+    <div className="loader">
+     
+    </div>
+  );
+};
+
+export default Loading;
